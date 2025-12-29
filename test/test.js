62f2f0b0a84f245bca5386e23b648b7a709bd754
Paul <paul@arcticnotes.com>
2025-12-29 01:51:35 -0500

    design a core model

--- test/test.js
+++ test/test.js
@@ -1,8 +1,16 @@
 import ASSERT from 'node:assert/strict';
+import PATH from 'node:path';
 import TEST from 'node:test';
-import { KeyframeParser} from '@arcticnotes/keyframe-core';
+import { Codec, ParseError} from '@arcticnotes/keyframe-core';
 
-TEST( 'smoke-test', async() => {
-	const parser = new KeyframeParser();
-	ASSERT.ok( parser);
+TEST( 'example.ank', async() => {
+	try {
+		const parser = new Codec();
+		const transitions = await parser.parseFile( PATH.join( import.meta.dirname, 'example.ank'));
+		ASSERT.ok( transitions);
+	} catch( error) {
+		if( error instanceof ParseError)
+			error.print();
+		throw error;
+	}
 });
